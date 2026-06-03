b47e6e7e898f8bc2cfd684176cc8706be38554d8
Abel Kristanto <58840455+AbelKristanto@users.noreply.github.com>
2026-06-03 20:23:50 +0700

    Change model file path for heart disease prediction

--- heart-disease.py
+++ heart-disease.py
@@ -93,7 +93,7 @@ def heart():
     if st.sidebar.button("GO!"):
         df = input_df.copy()
         st.write(df)
-        model = pickle.load(open('model/generate_heart_disease.pkl', 'rb'))
+        model = pickle.load(open('model/hasilgenerate.pkl', 'rb'))
         prediction = model.predict(df)
         result = ['No Heart Disease' if prediction == 0 else 'Heart Disease']
         with st.spinner('Wait for it...'):
